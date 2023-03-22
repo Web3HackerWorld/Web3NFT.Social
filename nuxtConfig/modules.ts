3a78a072a86992f7184328c9596b869b1e64a7b7
Web3HackerDAO <web3hackerdao@gmail.com>
2023-03-22 15:07:37 +0800

    ✨ add GTAG_ID setup

--- nuxtConfig/modules.ts
+++ nuxtConfig/modules.ts
@@ -1,40 +1,57 @@
-import type { NuxtConfig } from 'nuxt/config'
+const i18n = [
+  '@nuxtjs/i18n',
+  {
+    locales: [
+      {
+        code: 'en',
+        file: 'en_US.json',
+      },
+      {
+        code: 'zh',
+        file: 'zh_CN.json',
+      },
+    ],
+    lazy: true,
+    langDir: 'lang/',
+    defaultLocale: 'en',
+  },
+]
 
-export const modules: NuxtConfig['modules'] = [
-  [
-    '@nuxtjs/i18n',
-    {
-      locales: [
-        {
-          code: 'en',
-          file: 'en_US.json',
-        },
-        {
-          code: 'zh',
-          file: 'zh_CN.json',
-        },
-      ],
-      lazy: true,
-      langDir: 'lang/',
-      defaultLocale: 'en',
-    },
-  ],
+const unocss = [
+  '@unocss/nuxt',
+  {
+    // presets
+    uno: true, // enabled `@unocss/preset-uno`
+    icons: true, // enabled `@unocss/preset-icons`
+    attributify: true, // enabled `@unocss/preset-attributify`,
+    // core options
+    shortcuts: [],
+    rules: [],
+  },
+]
+
+const maybeUseModules = () => {
+  const modules = []
+  const { GTAG_ID } = process.env
+  if (GTAG_ID) {
+    modules.push([
+      'nuxt-gtag',
+      {
+        id: GTAG_ID,
+      },
+    ])
+  }
+
+  return modules
+}
+
+export const modules = [
+  ...maybeUseModules(),
+  i18n,
+  unocss,
   '@vueuse/motion/nuxt',
   'nuxt-lodash',
   '@vueuse/nuxt',
-  [
-    '@unocss/nuxt',
-    {
-    // presets
-      uno: true, // enabled `@unocss/preset-uno`
-      icons: true, // enabled `@unocss/preset-icons`
-      attributify: true, // enabled `@unocss/preset-attributify`,
-      // core options
-      shortcuts: [],
-      rules: [],
-    },
-
-  ],
   '@pinia/nuxt',
   '@nuxtjs/color-mode',
   '@vite-pwa/nuxt',
