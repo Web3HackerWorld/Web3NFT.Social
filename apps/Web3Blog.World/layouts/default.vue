15884b0f3bdbb894be5e06ed85b2dd6a03d13527
Web3HackerDAO <web3hackerdao@gmail.com>
2023-04-07 21:04:36 +0800

    💄 fix layout

--- apps/Web3Blog.World/layouts/default.vue
+++ apps/Web3Blog.World/layouts/default.vue
@@ -5,7 +5,9 @@
   <div min-h-screen flex flex-col bg-white>
     <TheHeader />
     <div flex-1 flex flex-col px-6 lg:px-8>
-      <slot />
+      <div mx-auto w-full max-w-3xl>
+        <slot />
+      </div>
     </div>
     <TheFooter />
     <BsLogin />
